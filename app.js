6e56bbd13ae932e2afaa5d1fdbc84653dac0b164
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-02-03 19:27:46 +0900

    4.4 Getting Username
    Added greetings message which appears when submit the username using input form.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.toggle(HIDDEN_CLASSNAME);
+
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.toggle(HIDDEN_CLASSNAME);
 }
 
 
